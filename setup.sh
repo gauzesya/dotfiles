1a4f3f0ad93f87366215ce6507866fbe9547cc59
gauzesya <gauzesya@gmail.com>
2019-07-01 18:50:52 +0100

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+PACKAGE='zsh vim-gnome tmux curl cmake git'
+
+if which apt-get >/dev/null 2>&1; then
+  LENGTH=`echo $PACKAGE | tr ' ' '\n' | wc -l`
+  for i in `seq $LENGTH`
+  do
+    ELEMENT=`echo $PACKAGE | cut -d ' ' -f $i`
+    sudo apt-get install $ELEMENT -y
+  done
+fi
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+export PATH="~/.pyenv/bin:$PATH"
+pyenv install anaconda3-5.3.1
+pyenv global anaconda3-5.3.1
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all --no-update-rc
