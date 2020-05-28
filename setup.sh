150de5e91f739019bcef171dd9b371dff58b9bfb
gauzesya <gauzesya@gmail.com>
2020-05-28 23:35:40 +0900

    change looking of setup.sh

--- setup.sh
+++ setup.sh
@@ -37,12 +37,14 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all --no-update-rc
 
 # Python
+echo ''
 if ask_yes_no "Install python settings ?"; then
   git clone https://github.com/pyenv/pyenv.git ~/.pyenv
   git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 fi
 
 # Rust
+echo ''
 if ask_yes_no "Install rust settings ?"; then
   curl https://sh.rustup.rs -sSf | sh
   rustup update
@@ -50,6 +52,7 @@ if ask_yes_no "Install rust settings ?"; then
 fi
 
 # Clojure
+echo ''
 if ask_yes_no "Install clojure settings ?"; then
   sudo apt-get install openjdk-8-jdk -y
   curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -o ~/bin/lein --create-dirs
