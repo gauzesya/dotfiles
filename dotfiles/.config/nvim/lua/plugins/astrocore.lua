cfe94eeb5024bde2f293ba43b4319e42911dc36c
gauzesya <gauzesya@gmail.com>
2025-03-02 18:03:02 +0900

    ➕ Add community package

--- dotfiles/.config/nvim/lua/plugins/astrocore.lua
+++ dotfiles/.config/nvim/lua/plugins/astrocore.lua
@@ -19,7 +19,7 @@ return {
     },
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {
-      virtual_text = true,
+      virtual_text = { spacing = 8 }, -- set virtual text spacing
       underline = true,
     },
     -- vim options can be configured here
